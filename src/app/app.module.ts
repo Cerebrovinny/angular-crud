530a7b432f123e3cda457228b0076d7b2d10c53b
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-01-01 10:47:39 -0300

    Added Firebase and Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDkClx0CWTC0HEo6Bcz7AJ5BQ2JcEIRCOU",
+      authDomain: "angular-crud-bf1b7.firebaseapp.com",
+      projectId: "angular-crud-bf1b7",
+      storageBucket: "angular-crud-bf1b7.appspot.com",
+      messagingSenderId: "662562520429",
+      appId: "1:662562520429:web:c59acc40fd570660946e64"
+    }),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
